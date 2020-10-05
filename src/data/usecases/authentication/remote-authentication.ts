38241f8810ce780dbb096aa35ded02e708dc80b6
unknown <viniciusbeloti7@gmail.com>
2020-10-05 18:36:29 -0300

    feat: ensure RemoteAuthentication calss HttpPostClient with correct body

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,3 +1,4 @@
+import { AuthenticationParams } from '../../../domain/usecases/authentication'
 import { HttpPostClient } from '../../protocols/http/http-post-client'
 
 export class RemoteAuthentication {
@@ -5,7 +6,7 @@ export class RemoteAuthentication {
     private readonly url: string,
     private readonly httPostClient: HttpPostClient) {}
 
-  async auth (): Promise<void> {
-    await this.httPostClient.post({ url: this.url })
+  async auth (params: AuthenticationParams): Promise<void> {
+    await this.httPostClient.post({ url: this.url, body: params })
   }
 }
