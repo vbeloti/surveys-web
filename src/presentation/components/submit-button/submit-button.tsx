045d9dc0ebe5c941b6109f517bf192b244bc835c
unknown <viniciusbeloti7@gmail.com>
2020-10-07 10:46:04 -0300

    feat: ensure Login sable submit button only if form is invalid

--- src/presentation/components/submit-button/submit-button.tsx
+++ src/presentation/components/submit-button/submit-button.tsx
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react'
-// import Context from '@/presentation/contexts/form/form-context'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = {
   text: string
 }
 
 const SubmitButton: React.FC<Props> = ({ text }: Props) => {
-  // const { state } = useContext(Context)
+  const { state } = useContext(Context)
   return (
     // <button data-testid="submit" disabled={state.isFormInvalid} type="submit">{text}</button>
-    <button data-testid="submit" disabled type="submit">Entrar</button>
+    <button data-testid="submit" disabled={state.emailError || state.passwordError} type="submit">{text}</button>
+    // <button data-testid="submit" disabled type="submit">Entrar</button>
   )
 }
 
