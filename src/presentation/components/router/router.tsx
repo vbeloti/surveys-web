00401f2ef43c2d35245ab47cff806b1504e2dbd8
unknown <viniciusbeloti7@gmail.com>
2020-10-07 17:52:07 -0300

    feat: add Login composition

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -4,17 +4,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 type Factory = {
   makeLogin: React.FC
-  makeSignUp: React.FC
+  // makeSignUp: React.FC
 }
 
 // const Router: React.FC<Factory> = (factory: Factory) => {
-const Router: React.FC = () => {
+const Router: React.FC<Factory> = ({ makeLogin }: Factory) => {
   return (
     <BrowserRouter>
       <Switch>
         {/* <Route path="/login" exact component={factory.makeLogin} />
         <Route path="/signup" exact component={factory.makeSignUp} /> */}
-        <Route path="/login" exact component={Login} />
+        <Route path="/login" exact component={makeLogin} />
       </Switch>
     </BrowserRouter>
   )
